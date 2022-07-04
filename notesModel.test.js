432d3cce1e2635abfd8c30a49b255df1698944cc
Clare Budds <clarebudds@yahoo.co.uk>
2022-07-04 19:19:47 +0100

    Test driving the model class

---
+++ notesModel.test.js
@@ -0,0 +1,23 @@
+const NotesModel = require("./notesModel");
+
+describe("NotesModel", () => {
+  it("should construct an empty array", () => {
+    const model = new NotesModel();
+    expect(model.getNotes()).toEqual([]);
+  });
+
+  it("should add notes to a list", () => {
+    const model = new NotesModel();
+    model.addNote("Buy milk");
+    model.addNote("Go to the gym");
+    expect(model.getNotes()).toEqual(["Buy milk", "Go to the gym"]);
+  });
+
+  it("should remove all notes in a list when it is reset", () => {
+    const model = new NotesModel();
+    model.addNote("Buy milk");
+    model.addNote("Go to the gym");
+    model.reset();
+    expect(model.getNotes()).toEqual([]);
+  });
+});
