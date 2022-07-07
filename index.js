a45996f96d5347026ef386927b241bcb8cbe264f
Clare Budds <clarebudds@yahoo.co.uk>
2022-07-07 16:00:38 +0100

    updated index.js file

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const NotesModel = require("./notesModel");
 const NotesView = require("./notesView");
 
 const model = new NotesModel();
-model.addNote("This is an example note");
-
 const view = new NotesView(model);
+model.addNote("A first note");
+model.addNote("Another one");
 
 view.displayNotes();
